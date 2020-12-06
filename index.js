d3f1df1e12748d6b3e6b19ca95923934cc6ea3f0
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-06 12:04:47 +0000

    Added a server code

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-12-06 12:04:18
+ * @modify date 2020-12-06 12:04:18
+ * @desc [description]
+ */
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => console.log("Server Listening at 3000"));
